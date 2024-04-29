21561e0c5c2e87e2355bcaab49edcf2cf7a27c3d
DanielJunkes <danielmafioleti2@gmail.com>
2024-04-29 20:47:11 -0300

    thread e classe nova

---
+++ src/main/java/org/example/ComParalelismo.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+import java.io.File;
+
+public class ComParalelismo {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Informe um Nome para a Busca");
+
+        File arquivos[];
+        File diretorio = new File("arquivoTXT");
+        arquivos = diretorio.listFiles();
+
+}
--- src/main/java/util/Threads/Thread.java
+++ src/main/java/util/Threads/Thread.java
@@ -1,4 +1,38 @@
 package util.Threads;
 
-public class Thread {
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Thread implements Runnable {
+
+    File arquivo;
+    String nome;
+
+    public Thread(File arquivo, String nome) {
+        this.arquivo = arquivo;
+        this.nome = nome;
+    }
+
+    public void run() {
+        long tempoInicial = System.currentTimeMillis();
+        long tempoFinal = 0;
+        try {
+            Scanner scan = new Scanner(arquivo);
+            int linha = 0;
+            while (scan.hasNextLine()) {
+                linha++;
+                if (nome.equals(scan.nextLine())) {
+                    JOptionPane.showMessageDialog(null,
+                            "Nome Encontrado" + "\nArquivo: " + arquivo.getName() + "\nLinha: " + linha);
+                    tempoFinal = System.currentTimeMillis();
+                }
+            }
+            scan.close();
+            System.out.println("O método foi executado em " + (tempoFinal - tempoInicial) + "ms");
+        } catch (FileNotFoundException e) {
+            JOptionPane.showConfirmDialog(null, "Arquivo não encontrado");
+        }
+    }
 }
