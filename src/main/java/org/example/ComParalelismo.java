de515cf9874acaa4a2fcd0c26859f4d910589e99
Thiago Serafina <thiilukly@outlook.com>
2024-04-29 21:47:14 -0300

    funcionando

--- src/main/java/org/example/ComParalelismo.java
+++ src/main/java/org/example/ComParalelismo.java
@@ -1,15 +1,49 @@
 package org.example;
 
+import util.Threads.Leitor;
+
 import javax.swing.*;
 import java.io.File;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ComParalelismo {
+    private static AtomicBoolean encontrado = new AtomicBoolean(false);
+
     public static void main(String[] args) {
         String nome = JOptionPane.showInputDialog("Informe um Nome para a Busca");
 
-        File arquivos[];
+        File[] arquivos;
         File diretorio = new File("arquivoTXT");
         arquivos = diretorio.listFiles();
 
+        long tempoInicial = System.currentTimeMillis();
+
+        assert arquivos != null;
+        Thread[] threads = new Thread[arquivos.length];
+
+        for (int i = 0; i < arquivos.length; i++) {
+            final File arquivo = arquivos[i];
+            threads[i] = new Thread(new Leitor(arquivo, nome, encontrado, tempoInicial));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (Thread thread : threads) {
+            if (thread.isAlive()) {
+                System.out.println("Thread " + thread.getName() + " não parou");
+                thread.interrupt();
+            }
+        }
+
+        if (!encontrado.get()) {
+            JOptionPane.showMessageDialog(null, "Nome não encontrado");
+        }
     }
 }
---
+++ src/main/java/util/Threads/Leitor.java
@@ -0,0 +1,48 @@
+package util.Threads;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Leitor implements Runnable {
+    private File arquivo;
+    private String nome;
+    private AtomicBoolean encontrado;
+    private long tempoInicial;
+
+    public Leitor(File arquivo, String nome, AtomicBoolean encontrado, long tempoInicial) {
+        this.arquivo = arquivo;
+        this.nome = nome;
+        this.encontrado = encontrado;
+        this.tempoInicial = tempoInicial;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (!encontrado.get()) {
+                Scanner scan = new Scanner(arquivo);
+                int linha = 0;
+                while (scan.hasNextLine()) {
+                    linha++;
+                    String linhaAtual = scan.nextLine();
+                    if (nome.equals(linhaAtual)) {
+                        encontrado.set(true);
+                        long tempoFinal = System.currentTimeMillis();
+                        System.out.println("O método foi executado em " + (tempoFinal - tempoInicial) + "ms");
+                        JOptionPane.showMessageDialog(null,
+                                "Nome Encontrado" + "\nArquivo: " + arquivo.getName() + "\nLinha: " + linha);
+
+                        break;
+                    }
+                }
+                scan.close();
+            }
+        } catch (FileNotFoundException e) {
+            JOptionPane.showConfirmDialog(null, "Arquivo não encontrado");
+        }
+    }
+}
+
