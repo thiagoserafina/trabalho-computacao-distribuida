2a794e6fbe0b900b31e9991843d6de4e867a8d29
DanielJunkes <danielmafioleti2@gmail.com>
2024-05-05 11:42:28 -0300

    pegando a linha

--- src/main/java/org/example/BuscaBinaria/ComParalelismo/Leitor.java
+++ src/main/java/org/example/BuscaBinaria/ComParalelismo/Leitor.java
@@ -1,13 +1,11 @@
 package org.example.BuscaBinaria.ComParalelismo;
 
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Leitor implements Runnable {
@@ -35,9 +33,10 @@ public class Leitor implements Runnable {
 
                 if (comparacao == 0) {
                     long tempoFinal = System.currentTimeMillis();
+                    int linha = pegarLinha(nome, arquivo.getPath());
                     encontrado.set(true);
                     JOptionPane.showMessageDialog(null,
-                            "Arquivo: " + arquivo.getName() + "\nLinha: " + "\nTempo de execução: " + (tempoFinal - tempoInicial) + "ms",
+                            "Arquivo: " + arquivo.getName() + "\nLinha: " + linha + "\nTempo de execução: " + (tempoFinal - tempoInicial) + "ms",
                             "Nome encontrado", JOptionPane.INFORMATION_MESSAGE);
                     break;
                 } else if (comparacao < 0) {
@@ -65,6 +64,21 @@ public class Leitor implements Runnable {
             return null;
         }
         return nomes;
+    }
+
+    private static int pegarLinha(String nome, String nomeArquivo) {
+        int linha = 0;
+        try (Scanner sc = new Scanner(new File(nomeArquivo))) {
+            while (sc.hasNextLine()) {
+                linha++;
+                if (sc.nextLine().equals(nome)) {
+                    return linha;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+        return -1;
     }
 }
 
--- src/main/java/org/example/BuscaBinaria/SemParalelismo/SemParalelismo.java
+++ src/main/java/org/example/BuscaBinaria/SemParalelismo/SemParalelismo.java
@@ -1,13 +1,11 @@
 package org.example.BuscaBinaria.SemParalelismo;
 
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class SemParalelismo {
 
@@ -31,12 +29,12 @@ public class SemParalelismo {
             if (nomesOrdenados != null) {
                 // Realiza a busca binária
                 int indice = buscaBinaria(nomesOrdenados, nome);
-
                 // Verifica se o nome foi encontrado
                 if (indice != -1) {
                     tempoFinal = System.currentTimeMillis();
+                    int linha = pegarLinha(nome ,arquivos[i].getPath());
                     JOptionPane.showMessageDialog(null,
-                            "Arquivo: " + arquivos[i].getName() + "\nLinha: " + "\nTempo de execução: " + (tempoFinal - tempoInicial) + "ms",
+                            "Arquivo: " + arquivos[i].getName() + "\nLinha: " + linha + "\nTempo de execução: " + (tempoFinal - tempoInicial) + "ms",
                             "Nome encontrado", JOptionPane.INFORMATION_MESSAGE);
                 }
             } else {
@@ -78,5 +76,20 @@ public class SemParalelismo {
             }
         }
         return -1; // Nome não encontrado
+    }
+
+    private static int pegarLinha(String nome, String nomeArquivo) {
+        int linha = 0;
+        try (Scanner sc = new Scanner(new File(nomeArquivo))) {
+            while (sc.hasNextLine()) {
+                linha++;
+                if (sc.nextLine().equals(nome)) {
+                    return linha;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+        return -1;
     }
 }
